dbabf65697afc5bfe9528bee5fcfbccfe38c6778
kritvikalani <67899380+kritvikalani@users.noreply.github.com>
2021-05-13 16:31:07 +0530

    Update SummaryScreen.js

--- screens/SummaryScreen.js
+++ screens/SummaryScreen.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import db from '../config';
 
-export default class SummaryScreen extends React.Component(){
+export default class SummaryScreen extends React.Component{
 constructor(){
   super();
   this.state = {
